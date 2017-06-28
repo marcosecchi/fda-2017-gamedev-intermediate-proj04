78ad3680f5fc23bac63fe6e6e2944b415b5b541c
Marco Secchi <loziosecchi@gmail.com>
2017-06-28 19:00:35 +0200

    Initial import.

---
+++ Assets/MoveAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveAI : MonoBehaviour {
+
+	public Transform target;
+	private NavMeshAgent _agent;
+
+	void Start () {
+		if (target == null)
+			target = GameObject.FindGameObjectWithTag ("Player").transform;
+		_agent = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	void Update () {
+		_agent.destination = target.position;
+				
+	}
+}
