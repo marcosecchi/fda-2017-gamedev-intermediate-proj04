368d02d3ff623424c8a6cc9a66dbe0bf0134a9af
Marco Secchi <loziosecchi@gmail.com>
2017-06-29 15:48:30 +0200

    Improve Sentinel AI.

---
+++ Assets/Scripts/Data/SentinelAIScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SentinelAIData", menuName = "FDA/Create Sentinel Data", order = 2)]
+public class SentinelAIScriptableObject : ScriptableObject {
+
+}
---
+++ Assets/Scripts/SentinelController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SentinelController : MonoBehaviour {
+
+	public SentinelAIScriptableObject data;
+
+	Camera _camera;
+
+	Plane[] _planes;
+
+	public Transform target;
+
+	Collider _targetCollider;
+
+	bool _targetInLOS;
+
+	void Start () {
+		_camera = GetComponent<Camera> ();
+		_planes = GeometryUtility.CalculateFrustumPlanes (_camera);
+
+		if (target == null)
+			target = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		_targetCollider = target.GetComponent<Collider> ();
+	}
+	
+	void Update () {
+		_targetInLOS = GeometryUtility.TestPlanesAABB (_planes, _targetCollider.bounds);
+
+		if (_targetInLOS) {
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), Time.deltaTime);
+			Debug.Log ("InLOS");
+		}
+			
+	}
+}
